59bd46dc5486f6305ed797f5d2cb86c2feac7313
PhoenixOrigin <phoenixorigin171@gmail.com>
2023-09-23 16:56:29 +0100

    maybe work

--- src/main/java/net/phoenix/api/commands/WebsocketCommand.java
+++ src/main/java/net/phoenix/api/commands/WebsocketCommand.java
@@ -1,5 +1,6 @@
 package net.phoenix.api.commands;
 
+import com.google.gson.JsonObject;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
@@ -18,24 +19,71 @@ import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.lit
 public class WebsocketCommand {
 
     private int execute(CommandContext<FabricClientCommandSource> ctx) {
-        MinecraftClient.getInstance().player.sendMessage(Text.literal("Incorrect usage of command"));
+        String token = APIClient.config.get("token");
+        MinecraftClient client = MinecraftClient.getInstance();
+        String username = client.player.getName().getString();
+        String uuid = client.player.getUuidAsString();
+
+        JsonObject obj = new JsonObject();
+        obj.addProperty("wsToken", token);
+        obj.addProperty("username", username);
+        obj.addProperty("uuid", uuid);
+        obj.addProperty("message", ctx.getArgument("content", String.class).replace("\n", "\\n"));
+        obj.addProperty("type", "chat");
+
+        APIClient.websocket.send(obj.toString());
         return 0;
     }
 
     private int terminate(CommandContext<FabricClientCommandSource> ctx) {
+        if(APIClient.websocket.isClosed()) {
+            MinecraftClient.getInstance().player.sendMessage(Text.literal("You are already connected to the server!"));
+        }
         APIClient.websocket.dc = true;
         APIClient.websocket.close();
         return 0;
     }
 
     private int connect(CommandContext<FabricClientCommandSource> ctx) {
+        if(APIClient.websocket.isOpen()) {
+            MinecraftClient.getInstance().player.sendMessage(Text.literal("You are already connected to the server!"));
+        }
         APIClient.websocket.dc = false;
         APIClient.connectWebsocket();
         return 0;
     }
 
     private int send(CommandContext<FabricClientCommandSource> ctx) {
-        APIClient.websocket.send(ctx.getArgument("content", String.class));
+        String token = APIClient.config.get("token");
+        MinecraftClient client = MinecraftClient.getInstance();
+        String username = client.player.getName().getString();
+        String uuid = client.player.getUuidAsString();
+
+        JsonObject obj = new JsonObject();
+        obj.addProperty("wsToken", token);
+        obj.addProperty("username", username);
+        obj.addProperty("uuid", uuid);
+        obj.addProperty("message", ctx.getArgument("content", String.class).replace("\n", "\\n"));
+        obj.addProperty("type", "manualSend");
+
+        APIClient.websocket.send(obj.toString());
+        return 0;
+    }
+
+    private int command(CommandContext<FabricClientCommandSource> ctx) {
+        String token = APIClient.config.get("token");
+        MinecraftClient client = MinecraftClient.getInstance();
+        String username = client.player.getName().getString();
+        String uuid = client.player.getUuidAsString();
+
+        JsonObject obj = new JsonObject();
+        obj.addProperty("wsToken", token);
+        obj.addProperty("username", username);
+        obj.addProperty("uuid", uuid);
+        obj.addProperty("message", ctx.getArgument("content", String.class).replace("\n", "\\n"));
+        obj.addProperty("type", "command");
+
+        APIClient.websocket.send(obj.toString());
         return 0;
     }
 
@@ -46,7 +94,11 @@ public class WebsocketCommand {
                 .then(literal("send")
                         .then(RequiredArgumentBuilder.argument("content", StringArgumentType.greedyString()))
                         .executes(this::send))
+                .then(literal("command")
+                        .then(RequiredArgumentBuilder.argument("content", StringArgumentType.greedyString()))
+                        .executes(this::command))
                 .then(literal("connect").executes(this::connect))
+                .then(RequiredArgumentBuilder.argument("content", StringArgumentType.greedyString()))
                 .executes(this::execute);
     }
 
