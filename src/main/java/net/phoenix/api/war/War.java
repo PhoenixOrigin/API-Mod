edef54f71508fb8047eb515acd5da1ac78729a71
PhoenixOrigin <phoenixorigin171@gmail.com>
2023-09-23 18:15:43 +0100

    maybe work

--- src/main/java/net/phoenix/api/war/War.java
+++ src/main/java/net/phoenix/api/war/War.java
@@ -1,19 +1,45 @@
 package net.phoenix.api.war;
 
+import net.minecraft.client.gui.hud.ClientBossBar;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class War {
 
     private List<String> warMembersUUID = new ArrayList<>();
     private String territoryName = "";
     private String territoryDefense = "";
+    private TerritoryDefenses start;
+    private TerritoryDefenses end;
+    private long startTime;
+    Pattern warBossBar = Pattern.compile("\\[([A-Z,a-z]{3,4})\\] ([A-Z,a-z ]*) - ❤ ([0-9]*) \\(([0-9,.,%]*)\\) - ☠ ([0-9]*)-([0-9]*) \\(([0-9,.]*)");
+
+
+    public War(ClientBossBar bossBar){
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void deserializeBossBar(ClientBossBar bossBar) {
+    }
+
+
 
     public class TerritoryDefenses {
         public long health;
-        public int attack1;
-        public int attack2;
+        public int attackLow;
+        public int attackHigh;
         public float defense;
+        public float atckSpeed;
+
+        public TerritoryDefenses(long health, int attackLow, int attackHigh, float defense, float atckSpeed) {
+            this.health = health;
+            this.attackLow = attackLow;
+            this.attackHigh = attackHigh;
+            this.defense = defense;
+            this.atckSpeed = atckSpeed;
+        }
 
     }
 
